b382c7ec6eec416bac639a426eada7b9fa5f0161
Swangop <jdominguezmoreira@gmail.com>
2024-09-02 23:25:11 +0200

    structure preparation, style testing, language button testing

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import i18n from './i18n';
+import { I18nextProvider } from 'react-i18next';
+import LanguageButton from './components/LanguageButton';
+const languages = [
+  { code: 'en', label: 'English' },
+  { code: 'es', label: 'Español' },
+];
+
+function App() {
+  const [language, setLanguage] = useState('en'); // default language
+
+  const handleLanguageChange = (lang: string) => {
+    i18n.changeLanguage(lang);
+    setLanguage(lang);
+  };
+  
+  return (
+    <I18nextProvider i18n={i18n}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {i18n.t('welcome')}
+          </p>
+        
+          <LanguageButton languages={languages} method={handleLanguageChange}></LanguageButton>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {i18n.t('learnReact')}
+          </a>
+        </header>
+      </div>
+    </I18nextProvider>
+  );
+}
+
+export default App;
---
+++ src/components/LanguageButton.tsx
@@ -0,0 +1,27 @@
+// languageSwitcher.tsx
+import React from 'react';
+
+interface LanguageSwitcherProps {
+  languages: { code: string; label: string }[];
+  method: Function;
+}
+
+const LanguageButton = ({ languages, method }: LanguageSwitcherProps) => {
+  
+  return (
+    <div>
+      {languages.map((lang) => (
+        <button
+          className='LanguageButton'
+          key={lang.code}
+          value={lang.code}
+          onClick={(e)=>{method(lang.code)}}
+          >
+          {lang.label}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default LanguageButton;
